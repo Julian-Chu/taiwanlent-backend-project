2ecbe3d96efb45dec8141790fd4e5eaef0a98ce9
JulianChu <yulang.chu@gmail.com>
2018-04-23 00:02:47 +0200

    init orm and first model

---
+++ models/PersonalUser.js
@@ -0,0 +1,66 @@
+const sequelize = require('../services/sequelize').sequelize;
+const Sequelize = require('sequelize');
+
+const PersonalUser = sequelize.define('user_personal',{
+  userId: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    field: 'user_personal_id'
+  },
+  password:{
+    type: Sequelize.STRING(50),
+  },
+  username:{
+    type: Sequelize.STRING(50)
+  },
+  email: {
+    type: Sequelize.STRING(320)
+  },
+  name:{
+    type: Sequelize.STRING(50)
+  },
+  phone:{
+    type: Sequelize.STRING(50)
+  },
+  city: {
+    type: Sequelize.STRING(50),
+    allowNull: false
+  },
+  occupation: {
+    type: Sequelize.STRING(50)
+  },
+  livingYearInGermany:{
+    type: Sequelize.INTEGER,
+    field: 'living_year_in_germany'
+  },
+  school: {
+    type: Sequelize.STRING(50)
+  },
+  workExperience_1: {
+    type: Sequelize.STRING(50),
+    field: 'work_experience_1'
+
+  },
+  workExperience_2: {
+    type: Sequelize.STRING(50),
+    field: 'work_experience_2'
+  },
+  workExperience_3: {
+    type: Sequelize.STRING(50),
+    field: 'work_experience_3'
+  },
+  german: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false
+  }
+
+
+},{
+  freezeTableName: true,
+  timestamps: false
+} );
+
+PersonalUser.findAll().then(users=>{
+  console.log(users)
+})
---
+++ services/sequelize.js
@@ -0,0 +1,30 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('Test','postgres','1234', {
+  host: 'localhost',
+  port: '5432',
+  dialect: 'postgres',
+  operatorsAliases: false,
+
+  schema: 'taiwanlent',
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+})
+
+module.exports = { sequelize:sequelize };
+
+//Or
+// onst sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname');
+
+sequelize
+  .authenticate()
+  .then(()=>{
+    console.log('Connection has been established successfully');
+  })
+  .catch(err=>{
+    console.error('Unable to connect to the database:',err);
+  });
