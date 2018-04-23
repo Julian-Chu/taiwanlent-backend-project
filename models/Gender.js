f0d57178325e326800d117d02b35bded1a7ef928
JulianChu <yulang.chu@gmail.com>
2018-04-23 23:58:37 +0200

    add Gender Model

---
+++ models/Gender.js
@@ -0,0 +1,22 @@
+const sequelize = require('../services/sequelize').sequelize;
+const Sequelize = require('sequelize');
+
+const Gender = sequelize.define('gender',{
+  genderId: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    field: 'gender_id'
+  },
+  gender:{
+    type: Sequelize.STRING(10),
+  }
+},{
+  freezeTableName: true,
+  timestamps: false
+} );
+
+Gender.findAll().then(genders=>{
+  console.log(genders);
+})
+
