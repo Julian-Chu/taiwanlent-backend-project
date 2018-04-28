0c58adad583727d564f1009215f3fed9fd984492
JulianChu <yulang.chu@gmail.com>
2018-04-28 23:17:18 +0200

    add get user by Id

--- server.js
+++ server.js
@@ -2,10 +2,21 @@ const express = require('express');
 const app = express();
 const PersonalUser = require('./models/PersonalUser');
 
-app.get("/users", (req,res)=>{
+app.get("/user", (req,res)=>{
   PersonalUser.findAll({})
     .then(users=> res.status(200).json(users));
 });
+
+app.get("/user/:id", (req,res)=>{
+  if(req.params.id != 0){
+    console.log(req.params.id);
+    PersonalUser.find({
+      userId: req.params.id,
+      attributes:{exclude: ['password']}
+    })
+    .then(user=>res.status(200).json(user));
+  }
+})
 
 
 app.listen(5000);
