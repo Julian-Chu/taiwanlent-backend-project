0be9f1f6728b8bbdc4f00ef09883ad58a4d8e6a1
JulianChu <yulang.chu@gmail.com>
2018-05-10 21:04:18 +0200

    passport: JWT and Local for business user done

--- services/passport.js
+++ services/passport.js
@@ -1,11 +1,47 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/key');
+const BusinessUser = require('../models/BusinessUser');
 
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const LocalStrategy = require('passport-local');
 
-passport.serializeUser((user,done)=>{
-  done(null, )
+const businessUserLocalLogin = new LocalStrategy(async (username, password, done) => {
+  try {
+    const user = await BusinessUser.findOne({
+      where: {
+        username: 'test_businessUser'
+      },
+      attributes: ['userId', 'username', 'password']
+    });
+    if (!user || passport !== user.dataValues.password) {
+      return done(null, false);
+    }
+    return done(null, user);    
+  } catch (err) {
+    return done(err);
+  }
+});
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+  secretOrKey: keys.jwtjwtSecretKey,
+  jsonWebTokenOptions :{
+    maxAge: "7d"
+  }
+};
+
+const businessUserJwtLogin = new JwtStrategy(jwtOptions, async (payload, done)=>{
+  try{
+    const user = await BusinessUser.findById(payload.sub);
+    if(!user) done(null , false);
+    else done(null, user);
+
+  }catch(err){
+    console.log('err', err);
+  }
 })
+
+passport.use('businessLocalLogin', businessUserLocalLogin);
+passport.use('businessJwtLogin', businessUserJwtLogin);
