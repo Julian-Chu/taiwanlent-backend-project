302edd9301f6e8ad1c2cacb083b94e1ac5fa828e
JulianChu <yulang.chu@gmail.com>
2018-05-17 21:18:38 +0200

    add test case for signin

--- tests/authRoute.test.js
+++ tests/authRoute.test.js
@@ -4,11 +4,14 @@
 const request = require('supertest');
 const sinon = require('sinon');
 const requireAuth = require('../middlewares/requireAuth');
-const express = require('express');
-var app = express();
 
-sinon.stub(requireAuth,'JWToken').callsFake((req, res, next)=>{console.log('stub'); req.user={test:'test'}; next();});
-require('../routes/authRoute')(app);
+sinon.stub(requireAuth,'LocalLogin')
+     .callsFake((req, res, next)=>
+     {
+       req.user={userId:'1', emailVerified: false}; 
+       next();
+      });
+const app = require('../server').app;
 
 describe('Authentication',()=>{
   describe('Get /auth/logout',()=>{
@@ -22,6 +25,34 @@ describe('Authentication',()=>{
         done();
       });
 
+    })
+  })
+
+  describe('Post /auth/business/signin',()=>{
+    it('returns token when successful signin',(done)=>{
+      request(app)
+      .post('/auth/business/signin')
+      .expect(200)
+      .expect(res=>{expect(res.body.token).toBeDefined();})
+      .end((err,res)=>{
+        if(err) done(err);
+        done();
+      })
+    })
+  })
+
+  describe('Post /auth/business/signup',()=>{
+    it('returns 400, when username is empty',(done)=>{
+      request(app)
+      .post('/auth/business/signup')
+      .send({})
+      .set('Accept', 'application/json')
+      .set('Content-Type','application/json')
+      .expect(400)
+      .end((err,res)=>{
+        if(err) return done(err);
+        done();
+      })
     })
   })
 
