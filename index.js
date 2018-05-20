a4e7db26250dcb19029d2c74a7e328472708cc16
yiwei <yiweihsutw@gmail.com>
2018-05-20 23:20:20 +0200

    remove sequelize

--- index.js
+++ index.js
@@ -12,6 +12,4 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-// models.sequelize.sync({ force: true }).then(() => {
-  app.listen(8081);
-// });
+app.listen(8081);
