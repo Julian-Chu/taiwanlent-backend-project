54ac756bd50d0d373196925dfdf7af385f9ea57b
JulianChu <yulang.chu@gmail.com>
2018-05-26 23:49:55 +0200

    add post in BusinessUserRoute

--- routes/BusinessUserRoute.js
+++ routes/BusinessUserRoute.js
@@ -3,6 +3,10 @@
 const requireAuth = require('../middlewares/requireAuth');
 module.exports = app=>{
   app.get('/api/businessuser', requireAuth.JWToken, (req,res)=>{
-    res.send({user: req.user});
+    return res.send({user: req.user});
+  });
+
+  app.post('/api/businessuser', requireAuth.JWToken, (req,res)=>{
+    return res.status(204).send({});
   })
 }
