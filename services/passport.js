20518573bbe239b5796fb00635621ba48821e93b
JulianChu <yulang.chu@gmail.com>
2018-07-16 23:26:26 +0200

    start to implment business user google login

--- services/passport.js
+++ services/passport.js
@@ -4,10 +4,12 @@ const passport = require('passport');
 const keys = require('../config/key');
 const models = require('../models/index');
 const BusinessUser = models.UserBusiness;
+const GeneralUser = models.UserGeneral;
 
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const LocalStrategy = require('passport-local').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const dic = require('../dic');
 const bcrypt = require('bcrypt');
 const utils = require('../utils');
@@ -54,6 +56,33 @@ const businessUserJwtLogin = new JwtStrategy(jwtOptions, async (payload, done) =
   }
 })
 
+const BusinessUserGoogleLogin = new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback',
+    proxy: true
+  },
+  async (accessToken, refreshtoken, profile, done) => {
+    const existingUser = await GeneralUser.findOne({
+      where: {
+        google_id: profile.id
+      },
+      attributes: ['user_id', 'google_id', 'user_business_id', 'user_personal_id']
+    })
 
+    if (existingUser) {
+      done(null, existingUser);
+    } else {
+      // BusinessUser.create({})
+      // GeneralUser.create({
+      //   google_id: profile.id
+      // }).then(user => {
+      //   console.log(user.get({
+      //     plain: true
+      //   }));
+      // })
+    }
+  }
+)
 passport.use(dic.businessLocalLogin, businessUserLocalLogin);
 passport.use(dic.businessJwtLogin, businessUserJwtLogin);
