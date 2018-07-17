56c858322f90331957737a9663330e20465a69e4
JulianChu <yulang.chu@gmail.com>
2018-07-17 23:54:13 +0200

    add schema prefix

--- models/GeneralUser.js
+++ models/GeneralUser.js
@@ -1,6 +1,6 @@
 /* jshint indent: 2 */
 
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   return sequelize.define('user_general', {
     user_id: {
       type: DataTypes.INTEGER,
@@ -35,6 +35,7 @@ module.exports = function(sequelize, DataTypes) {
       }
     }
   }, {
-    tableName: 'user_general'
+    tableName: 'user_general',
+    schema: "taiwanlent"
   });
 };
