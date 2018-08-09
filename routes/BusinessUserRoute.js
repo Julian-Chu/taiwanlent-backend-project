535bc5f7c6c36d57ebd0b7624032b12c6a019e8a
JulianChu <yulang.chu@gmail.com>
2018-08-09 23:29:47 +0200

    fix bug

--- routes/BusinessUserRoute.js
+++ routes/BusinessUserRoute.js
@@ -1,18 +1,18 @@
 // @ts-check
 'use strict'
 const requireAuth = require('../middlewares/requireAuth');
+const models = require('../models/index');
 module.exports = app => {
   //取得用戶資料
   app.get('/api/businessuser', requireAuth.JWToken, (req, res) => {
     console.log('get user data');
-    return res.send({
-      user: req.user
-    });
+    return res.send(req.user);
   });
 
   //用戶註冊資料  
   app.post('/api/businessuser', requireAuth.JWToken, (req, res) => {
-    return res.status(201).send({});
+    console.log(req.body);
+    return res.status(204).send({});
   })
 
   //用戶修改資料
