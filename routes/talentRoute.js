fd896c8bf16cf307d3278cf3f1d45fe6d30b0647
JulianChu <yulang.chu@gmail.com>
2018-08-26 01:02:06 +0200

    implment query talents route

--- routes/talentRoute.js
+++ routes/talentRoute.js
@@ -1,16 +1,32 @@
 // @ts-check
 'use strict'
+const requireAuth = require("../middlewares/requireAuth");
+const models = require('../models/index');
 
 module.exports = app => {
-  
-  // 當沒有token或token.verified != true,  回傳人才部分資訊
+
+  // 當沒有token或token.verified != true,  回傳人才部分資訊(todo)
   // 針對已驗證的登入使用者回傳全部人才資訊
-  app.get('/api/talents',()=>{
-    
+  app.get("/api/talents", requireAuth.JWToken, async (req, res) => {
+    try {
+
+      let talents = await models.PersonalUser.findAll({
+        where: {
+          resume_open: true
+        }
+      })
+      console.log(talents);
+
+      return res.status(200).send(talents);
+    } catch (err) {
+      console.log('err:', err);
+
+    }
+
   });
 
   // 針對選定的人才寄送email
-  app.post('/api/candidates/message',()=>{
+  app.post('/api/candidates/message', () => {
 
   })
 }
