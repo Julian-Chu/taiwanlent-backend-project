1dab4d9ccc066af7447c208acc211a340874307e
JulianChu <yulang.chu@gmail.com>
2018-09-03 23:06:20 +0200

    change config

--- config/ci.js
+++ config/ci.js
@@ -1,3 +1,11 @@
 module.exports = {
-  connectionString: 'postgres://postgres:1234@localhost:5432/postgres'
+  jwtSecretKey: process.env.jwtSecretKey,
+  vtokenKey: process.env.vtokenKey,
+  connectionString: process.env.connectionString,
+  sendGridKey: process.env.sendGridKey,
+  redirectDomain: 'http://localhost:5000',
+  googleClientID_business: process.env.googleClientID_business,
+  googleClientSecret_business: process.env.googleClientSecret_business,
+  googleClientID_personal: process.env.googleClientID_personal,
+  googleClientSecret_personal: process.env.googleClientSecret_personal,
 }
---
+++ config/prod.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // postgresURI: 'postgresql://test:1234@localhost:5433',
+  jwtSecretKey: process.env.jwtSecretKey,
+  vtokenKey: process.env.vtokenKey,
+  connectionString: process.env.connectionString,
+  sendGridKey: process.env.sendGridKey,
+  redirectDomain: 'http://localhost:5000',
+  googleClientID_business: process.env.googleClientID_business,
+  googleClientSecret_business: process.env.googleClientSecret_business,
+  googleClientID_personal: process.env.googleClientID_personal,
+  googleClientSecret_personal: process.env.googleClientSecret_personal,
+
+}
