e444ad26384901c9aeffb7bc54930c2c5bf8cbce
JulianChu <yulang.chu@gmail.com>
2018-09-07 00:00:51 +0200

    add cluster

--- index.js
+++ index.js
@@ -5,18 +5,28 @@ const app = express();
 const bodyParser = require('body-parser');
 const models = require('./models/index');
 const passportConfig = require('./services/passport');
+const cluster = require('cluster');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+if (cluster.isMaster) {
+  cluster.fork();
 
 
-// Routes
-require('./routes/TalentRoute')(app);
-require('./routes/AuthRoute')(app);
-require('./routes/BusinessUserRoute')(app);
-require('./routes/PeronalUserRoute')(app);
-console.log("Server started: port 5000");
-module.exports.app = app;
-app.listen(5000);
+} else {
+
+
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({
+    extended: false
+  }));
+
+
+  // Routes
+  require('./routes/TalentRoute')(app);
+  require('./routes/AuthRoute')(app);
+  require('./routes/BusinessUserRoute')(app);
+  require('./routes/PeronalUserRoute')(app);
+  console.log("Server started: port 5000");
+  module.exports.app = app;
+  app.listen(5000);
+
+}
