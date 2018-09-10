f9e058ce2b64ec20c0c9c194ef589ccd24407282
JulianChu <yulang.chu@gmail.com>
2018-09-10 17:53:11 +0200

    add port from env

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 // @ts-check
-'use strict';
-const express = require('express');
+"use strict";
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const models = require('./models/index');
-const passportConfig = require('./services/passport');
+const bodyParser = require("body-parser");
+const models = require("./models/index");
+const passportConfig = require("./services/passport");
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
 
+let port = process.env.PORT || 5000;
 
 // Routes
-require('./routes/TalentRoute')(app);
-require('./routes/AuthRoute')(app);
-require('./routes/BusinessUserRoute')(app);
-require('./routes/PeronalUserRoute')(app);
+require("./routes/TalentRoute")(app);
+require("./routes/AuthRoute")(app);
+require("./routes/BusinessUserRoute")(app);
+require("./routes/PeronalUserRoute")(app);
 console.log("Server started: port 5000");
 module.exports.app = app;
-app.listen(5000);
+
+app.listen(port);
